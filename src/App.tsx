89a13b9a0da914066b4e476c7f7a632184dffe14
freixedelo <rui.freixedelo@ostmodern.co.uk>
2024-03-31 23:06:47 +0100

    improved data visualization, added date filters and date logs

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,29 @@ import axios from "axios";
 import { XMLParser } from "fast-xml-parser";
 import { useEffect, useState } from "react";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs, { Dayjs } from "dayjs";
 
 interface Equipment {
   b1Name: string;
   b2Name: string;
   b3Text: string;
+  intervalDefinition: {
+    intervalStart: {
+      date: string;
+      time: string;
+    };
+    intervalEnd: {
+      date: string;
+      time: string;
+    };
+  };
+}
+
+interface DateLog {
+  timestamp: string;
   intervalDefinition: {
     intervalStart: {
       date: string;
@@ -40,7 +58,7 @@ interface Ticket {
   availability: string;
   status: string;
   cause: string;
-  equipment: Equipment[];
+  equipment: Equipment[] | Equipment;
   statusLogs: {
     statusLog: {
       status: string;
@@ -48,19 +66,7 @@ interface Ticket {
     }[];
   };
   dateLogs: {
-    datelog: {
-      timestamp: string;
-      intervalDefinition: {
-        intervalStart: {
-          date: string;
-          time: string;
-        };
-        intervalEnd: {
-          date: string;
-          time: string;
-        };
-      };
-    }[];
+    datelog: DateLog[] | DateLog;
   };
 }
 
@@ -69,6 +75,8 @@ interface Filter {
   outageType: string;
   voltage: string;
   equipment: string;
+  equipmentStartDate: Dayjs | null;
+  equipmentEndDate: Dayjs | null;
 }
 
 function App() {
@@ -83,16 +91,14 @@ function App() {
     });
   }, []);
 
-  if (filteredData.length === 0 && xmlData) {
-    setFilteredData(xmlData.slice(0, 15));
-  }
-
   const { control, handleSubmit } = useForm({
     defaultValues: {
       outageStatus: "",
-      outageType: "",
+      outageType: " ",
       voltage: "",
       equipment: "",
+      equipmentStartDate: null,
+      equipmentEndDate: null,
     },
   });
 
@@ -101,7 +107,10 @@ function App() {
     outageType,
     voltage,
     equipment,
+    equipmentStartDate,
+    equipmentEndDate,
   }) => {
+    console.log(equipmentStartDate);
     let filteredData: Ticket[] = [];
 
     if (xmlData && outageStatus) {
@@ -135,55 +144,93 @@ function App() {
       );
     }
 
+    //(startdate <= interval_end_datetime) &  (enddate >= interval_start_datetime)
+
+    if (xmlData && equipmentStartDate && equipmentEndDate) {
+      filteredData = xmlData.filter(
+        (ticket) =>
+          Array.isArray(ticket?.equipment) &&
+          ticket?.equipment?.find((e) => {
+            const intervalEndDate = dayjs(
+              e?.intervalDefinition.intervalEnd.date
+            );
+            const intervalStartDate = dayjs(
+              e?.intervalDefinition.intervalEnd.date
+            );
+            return (
+              intervalEndDate.isAfter(equipmentStartDate) &&
+              intervalStartDate.isBefore(equipmentEndDate)
+            );
+          })
+      );
+    }
+
     console.log({ filteredData });
 
     setFilteredData(filteredData);
   };
 
+  if (!xmlData)
+    return (
+      <div className="w-full h-screen text-5xl text-center content-center uppercase font-semibold">
+        Loading
+      </div>
+    );
+
   return (
     <Container maxWidth="xl">
-      <h1 className="text-center font-semibold uppercase my-3 text-2xl">
+      <h1 className="text-center font-semibold uppercase my-5 text-2xl">
         {"Caravela Project"}
       </h1>
       <section>
         <h3>Filters</h3>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Controller
-            name="outageStatus"
-            control={control}
-            render={({ field }) => (
-              <TextField {...field} placeholder="Outage status" />
-            )}
-          />
+          <div className="my-2 space-x-3">
+            <Controller
+              name="outageStatus"
+              control={control}
+              render={({ field }) => (
+                <TextField {...field} placeholder="Outage status" />
+              )}
+            />
 
-          <Controller
-            name="outageType"
-            control={control}
-            render={({ field }) => (
-              <Select
-                {...field}
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Select"
-                placeholder="Select"
+            <Controller
+              name="outageType"
+              control={control}
+              render={({ field }) => (
+                <>
+                  <InputLabel id="outage-type-select-label">
+                    Outage Type
+                  </InputLabel>
+                  <Select
+                    {...field}
+                    labelId="outage-type-select-label"
+                    id="outage-type-select"
+                    label="Select"
+                    onChange={(e) => {
+                      field.onChange(e);
+                    }}
+                  >
+                    <MenuItem value=" ">
+                      <em>Any</em>
+                    </MenuItem>
+                    <MenuItem value={"Continuous"}>Continuous</MenuItem>
+                    <MenuItem value={"DailyNoWeekends"}>
+                      Daily No Weekends
+                    </MenuItem>
+                    <MenuItem value={"ContinuousNoWeekends"}>
+                      Continuous No Weekends
+                    </MenuItem>
+                    <MenuItem value={"DailyIncludingWeekends"}>
+                      Daily Including Weekends
+                    </MenuItem>
+                  </Select>
+                </>
+              )}
+            />
+          </div>
 
-                //onChange={handleChange}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={"Continuous"}>Continuous</MenuItem>
-                <MenuItem value={"DailyNoWeekends"}>Daily No Weekends</MenuItem>
-                <MenuItem value={"ContinuousNoWeekends"}>
-                  Continuous No Weekends
-                </MenuItem>
-                <MenuItem value={"DailyIncludingWeekends"}>
-                  Daily Including Weekends
-                </MenuItem>
-              </Select>
-            )}
-          />
-          <div className="my-2">
+          <div className="my-2 space-x-3">
             <Controller
               name="voltage"
               control={control}
@@ -198,23 +245,52 @@ function App() {
                 <TextField {...field} placeholder="Equipment" />
               )}
             />
+
+            <Controller
+              name="equipmentStartDate"
+              control={control}
+              render={({ field }) => (
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    value={field.value}
+                    ref={field.ref}
+                    onChange={(date) => {
+                      field.onChange(date);
+                    }}
+                    label="Start date"
+                  />
+                </LocalizationProvider>
+              )}
+            />
+
+            <Controller
+              name="equipmentEndDate"
+              control={control}
+              render={({ field }) => (
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    value={field.value}
+                    ref={field.ref}
+                    onChange={(date) => {
+                      field.onChange(date);
+                    }}
+                    label="End date"
+                  />
+                </LocalizationProvider>
+              )}
+            />
           </div>
           <Button variant="contained" type="submit">
             Filter
           </Button>
         </form>
-        {/** 
-         * 
-Filtro para entradas de equipamento:
--Equipment status
--Start date e end date:  (startdate <= interval_end_datetime) &  (enddate >= interval_start_datetime)
-  */}
       </section>
-      <section className="my-4">
-        <div>{`Showing ${filteredData.length} results`}</div>
 
-        <TableContainer component={Paper}>
-          <Table>
+      <section className="my-4 overflow-hidden">
+        <div className="my-4 text-right">{`Showing ${filteredData.length} results`}</div>
+
+        <TableContainer component={Paper} sx={{ maxHeight: 650 }}>
+          <Table stickyHeader>
             <TableHead>
               <TableRow>
                 <TableCell>pjmTicketId</TableCell>
@@ -228,66 +304,99 @@ Filtro para entradas de equipamento:
               </TableRow>
             </TableHead>
             <TableBody>
-              {filteredData.map((ticket: Ticket) => (
-                <TableRow
-                  key={ticket.pjmTicketId}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {ticket.pjmTicketId}
-                  </TableCell>
-                  <TableCell align="right">{ticket.companyName}</TableCell>
-                  <TableCell align="right">{ticket.outageType}</TableCell>
-                  <TableCell align="right">{ticket.status}</TableCell>
-                  <TableCell align="right">{ticket.cause}</TableCell>
-                  <TableCell sx={{ minWidth: 300 }}>
-                    {ticket?.equipment?.map((e, index) => {
-                      const { intervalDefinition, b1Name, b2Name, b3Text } = e;
-                      const start = new Date(
-                        `${intervalDefinition?.intervalStart.date}T${intervalDefinition?.intervalStart.time}Z`
-                      );
-                      const end = new Date(
-                        `${intervalDefinition?.intervalEnd.date}T${intervalDefinition?.intervalEnd.time}Z`
-                      );
-                      const duration = end.getTime() - start.getTime();
+              {filteredData.map((ticket: Ticket) => {
+                return (
+                  <TableRow
+                    key={ticket.pjmTicketId}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                      verticalAlign: "top",
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {ticket.pjmTicketId}
+                    </TableCell>
+                    <TableCell align="right">{ticket.companyName}</TableCell>
+                    <TableCell align="right">{ticket.outageType}</TableCell>
+                    <TableCell align="right">{ticket.status}</TableCell>
+                    <TableCell align="right">{ticket.cause}</TableCell>
+                    <TableCell>
+                      {Array.isArray(ticket?.equipment) ? (
+                        ticket?.equipment?.map((e, index) => {
+                          const { intervalDefinition, b1Name, b2Name, b3Text } =
+                            e;
+                          const start = new Date(
+                            `${intervalDefinition?.intervalStart.date}T${intervalDefinition?.intervalStart.time}Z`
+                          );
+                          const end = new Date(
+                            `${intervalDefinition?.intervalEnd.date}T${intervalDefinition?.intervalEnd.time}Z`
+                          );
+                          const duration = end.getTime() - start.getTime();
 
-                      return (
-                        <div
-                          className="py-3 border-b border-b-slate-500"
-                          key={` ${b1Name}-${index}`}
-                        >
-                          {`${b2Name} - ${b1Name}`}
-                          <br />
-                          {b3Text}
-                          <br />
-                          <small>
-                            {`Start: ${intervalDefinition?.intervalStart.date} ${intervalDefinition?.intervalStart.time}`}
-                            <br />
-                            {`End: ${intervalDefinition?.intervalEnd.date} ${intervalDefinition?.intervalEnd.time}`}
-                            <br />
-                            {` Duration: ${duration / (1000 * 60 * 60)} h`}
-                          </small>
-                          <br />
-                        </div>
-                      );
-                    })}
-                  </TableCell>
-                  <TableCell sx={{ minWidth: 250 }}>
-                    {ticket?.statusLogs?.statusLog?.map((log, index) => {
-                      const { status, statusTime } = log;
+                          return (
+                            <div
+                              className="py-3 border-b border-b-slate-500"
+                              key={` ${b1Name}-${index}`}
+                            >
+                              {`${b2Name} - ${b1Name}`}
+                              <br />
+                              {b3Text}
+                              <br />
+                              <small>
+                                {`Start: ${intervalDefinition?.intervalStart.date} ${intervalDefinition?.intervalStart.time}`}
+                                <br />
+                                {`End: ${intervalDefinition?.intervalEnd.date} ${intervalDefinition?.intervalEnd.time}`}
+                                <br />
+                                {` Duration: ${duration / (1000 * 60 * 60)} h`}
+                              </small>
+                              <br />
+                            </div>
+                          );
+                        })
+                      ) : (
+                        <></>
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      {ticket?.statusLogs?.statusLog?.map((log, index) => {
+                        const { status, statusTime } = log;
 
-                      return (
-                        <div
-                          className="py-3 border-b border-b-slate-500"
-                          key={` -${index}`}
-                        >
-                          {`${status} - ${statusTime}`}
-                        </div>
-                      );
-                    })}
-                  </TableCell>
-                </TableRow>
-              ))}
+                        return (
+                          <div
+                            className="py-3 border-b border-b-slate-500"
+                            key={` -${index}`}
+                          >
+                            {`${status} - ${statusTime}`}
+                          </div>
+                        );
+                      })}
+                    </TableCell>
+
+                    <TableCell>
+                      {Array.isArray(ticket?.dateLogs?.datelog) ? (
+                        ticket?.dateLogs?.datelog?.map((log, index) => {
+                          const { intervalDefinition, timestamp } = log;
+
+                          return (
+                            <div
+                              className="py-3 border-b border-b-slate-500"
+                              key={` -${index}`}
+                            >
+                              {`Start: ${intervalDefinition?.intervalStart.date} ${intervalDefinition?.intervalStart.time}`}
+                              <br />
+                              {`End: ${intervalDefinition?.intervalEnd.date} ${intervalDefinition?.intervalEnd.time}`}
+                              <br />
+                              {`Timestamp: ${timestamp} `}
+                            </div>
+                          );
+                        })
+                      ) : (
+                        <></>
+                      )}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
             </TableBody>
           </Table>
         </TableContainer>
